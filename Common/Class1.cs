9d52091615c2cc88798447e2fa104bbdf78e2597
sys.peshkov <sys.peshkov@gmail.com>
2022-12-30 17:06:51 +0300

    Create methods InputNumber And InputString

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -1,5 +1,25 @@
 ﻿namespace Common;
 public class Class1
 {
+public static int InputNumber(string name)
+        {
+            Console.WriteLine(name);
+            string number = Console.ReadLine();
+            bool isParsed = int.TryParse(number, out int result);
 
+            if (!isParsed)
+            {
+                Console.WriteLine($"Вы ввели некорректное число, ожидается целое число, вы ввели {number}");
+                return -1;
+            }
+            return result;
+        }
+
+ public static string InputString(string name)
+        {
+            Console.WriteLine(name);
+            string element = Console.ReadLine();
+            
+            return element;
+        }       
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using static Common.Class1;
+Console.Clear();
+ 
+ int number = InputNumber("Введите число");
+ 
+    
