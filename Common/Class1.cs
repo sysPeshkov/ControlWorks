84a26734d6972d5764bf9805ec643ea4999db2c4
sys.peshkov <sys.peshkov@gmail.com>
2022-12-30 17:34:38 +0300

    Add methods for Input array, output array and test's template

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -1,25 +1,52 @@
 ﻿namespace Common;
 public class Class1
 {
-public static int InputNumber(string name)
-        {
-            Console.WriteLine(name);
-            string number = Console.ReadLine();
-            bool isParsed = int.TryParse(number, out int result);
+    public static int InputNumber(string name)
+    {
+        Console.WriteLine(name);
+        string number = Console.ReadLine();
+        bool isParsed = int.TryParse(number, out int result);
 
-            if (!isParsed)
-            {
-                Console.WriteLine($"Вы ввели некорректное число, ожидается целое число, вы ввели {number}");
-                return -1;
-            }
-            return result;
+        if (!isParsed)
+        {
+            Console.WriteLine($"Вы ввели некорректное число, ожидается целое число, вы ввели {number}");
+            return -1;
+        }
+        return result;
+    }
+
+    public static string InputString(string name)
+    {
+        Console.WriteLine(name);
+        string element = Console.ReadLine();
+
+        return element;
+    }
+
+    public static string[] FillStringArray(int arraySize)
+    {
+        string[] arrayResult = new string[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.WriteLine($"Введите элемент с индексом {i}");
+            arrayResult[i] = Console.ReadLine();
         }
 
- public static string InputString(string name)
+        return arrayResult;
+    }
+
+    public static void Print2DStringArray(string[,] array)
+    {
+        int numberRows = array.GetLength(0);
+        int numberColumns = array.GetLength(1);
+        for (int i = 0; i < numberRows; i++)
         {
-            Console.WriteLine(name);
-            string element = Console.ReadLine();
-            
-            return element;
-        }       
+            for (int j = 0; j < numberColumns; j++)
+            {
+                Console.Write($"{array[i, j]}");
+            }
+            Console.WriteLine();
+        }
+
+    }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,6 +1,33 @@
-﻿ using static Common.Class1;
+﻿using static Common.Class1;
 Console.Clear();
- 
- int number = InputNumber("Введите число");
- 
-    
+
+Tests(1);
+Tests(2);
+Tests(3);
+
+int arraySize = InputNumber("Введите размер массива");
+
+
+
+void Tests(int numberTest)
+{
+    if (numberTest == 1)
+    {
+        string[] array = new string[4] { "hello", "2", "world", ":-)" };
+        string[,] expectedResult = new string[2, 4] { { "hello", "2", "world", ":-)" }, { "1", "0", "1", "0" } };
+    }
+    if (numberTest == 2)
+    {
+        string[] array = new string[4] { "1234", "1567", "-2", "computer since" };
+        string[,] expectedResult = new string[2, 4] { { "1234", "1567", "-2", "computer since" }, { "1", "1", "0", "1" } };
+    }
+    if (numberTest == 3)
+    {
+        string[] array = new string[3] { "Russia", "Denmark", "Kazan" };
+        string[,] expectedResult = new string[2, 3] { { "Russia", "Denmark", "Kazan" }, { "1", "1", "1" } };
+    }
+    else
+    {
+        Console.WriteLine("Тесты не были запущены");
+    }
+}
