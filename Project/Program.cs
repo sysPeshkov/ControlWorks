0bbec08e76fc96c5965c71b8de5dc496bdec43c3
sys.peshkov <sys.peshkov@gmail.com>
2022-12-30 21:46:45 +0300

    Done method GetMoreOrLessElementsFromArray

--- Project/Program.cs
+++ Project/Program.cs
@@ -10,25 +10,46 @@ int arraySize = InputNumber("Введите размер массива");
 //Console.WriteLine("Ваш массив:");
 //PrintStringArray(array);
 
-string[,] GetMoreOrLessElementsFromArray(string)
-{}
+string[,] GetMoreOrLessElementsFromArray(string[] array, int amountSymbols)
+{
+    int isAmountLess = 0;
+    int isAmountMore = 1;
+    int amountColumns = array.Length;
+    string[,] resultArray = new string[2, amountColumns];
+    bool moreThanamountSymbols = false;
+    for (int i = 0; i < amountColumns; i++)
+    {
+        resultArray[0, i] = array[i];
+        moreThanamountSymbols = IsLongMoreThanNumber(array[i], amountSymbols);
+        if (!moreThanamountSymbols)
+        {
+            resultArray[1, i] = isAmountLess.ToString();
+        }
+        else
+        {
+            resultArray[1, i] = isAmountMore.ToString();
+        }
+    }
+    return resultArray;
+}
 
 
 
 bool test = IsLongMoreThanNumber("TestProverka", 3);
 Console.WriteLine(test);
 //Функция "Длина текста строго больше чем заданное число?"
-bool IsLongMoreThanNumber(string text, int number){
-bool result = false;
-//Длина входящего текста
-int length = text.Length;
-//Если длина текста больше чем заданное число,
-// то результат true
-if (length > number)
+bool IsLongMoreThanNumber(string text, int number)
 {
-    result = true;
-}
-return result;
+    bool result = false;
+    //Длина входящего текста
+    int length = text.Length;
+    //Если длина текста больше чем заданное число,
+    // то результат true
+    if (length > number)
+    {
+        result = true;
+    }
+    return result;
 }
 
 
@@ -50,7 +71,7 @@ void Tests(int numberTest)
         string[] array = new string[3] { "Russia", "Denmark", "Kazan" };
         string[,] expectedResult = new string[2, 3] { { "Russia", "Denmark", "Kazan" }, { "1", "1", "1" } };
     }
-    if (numberTest != 1 && numberTest != 2 && numberTest != 3) 
+    if (numberTest != 1 && numberTest != 2 && numberTest != 3)
     {
         Console.WriteLine("Тесты не были запущены");
     }
