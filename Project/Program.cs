95d959123f02916b66ecbb8e216582d899671039
sys.peshkov <sys.peshkov@gmail.com>
2022-12-30 21:19:40 +0300

    Create method GetMoreOrLessElementsFromArray

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,14 +6,28 @@ Tests(2);
 Tests(3);
 
 int arraySize = InputNumber("Введите размер массива");
-string[] array = FillStringArray(arraySize);
-Console.WriteLine("Ваш массив:");
-PrintStringArray(array);
+//string[] array = FillStringArray(arraySize);
+//Console.WriteLine("Ваш массив:");
+//PrintStringArray(array);
 
+string[,] GetMoreOrLessElementsFromArray(string)
+{}
+
+
+
+bool test = IsLongMoreThanNumber("TestProverka", 3);
+Console.WriteLine(test);
+//Функция "Длина текста строго больше чем заданное число?"
 bool IsLongMoreThanNumber(string text, int number){
 bool result = false;
+//Длина входящего текста
 int length = text.Length;
-
+//Если длина текста больше чем заданное число,
+// то результат true
+if (length > number)
+{
+    result = true;
+}
 return result;
 }
 
@@ -36,7 +50,7 @@ void Tests(int numberTest)
         string[] array = new string[3] { "Russia", "Denmark", "Kazan" };
         string[,] expectedResult = new string[2, 3] { { "Russia", "Denmark", "Kazan" }, { "1", "1", "1" } };
     }
-    else
+    if (numberTest != 1 && numberTest != 2 && numberTest != 3) 
     {
         Console.WriteLine("Тесты не были запущены");
     }
